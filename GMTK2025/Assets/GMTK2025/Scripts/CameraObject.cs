5391d1b954e2771d0d1b2c28e290f7f9f0a0da39
Henry Hammer <hammerthenry@gmail.com>
2025-07-30 17:36:43 -0400

    Camera follows player, rigidbody, hallway

---
+++ GMTK2025/Assets/GMTK2025/Scripts/CameraObject.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class CameraObject : MonoBehaviour //naming this file "camera" broke a ton of shit
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    [SerializeField] private Transform player;
+    [SerializeField] private Vector3 offset;
+
+    void LateUpdate()
+    {
+        if (player != null)
+        {
+            transform.position = player.position + offset;
+        }
+    }
+}
